60e5dac91f40c27c454540464b31209caae530dc
Dipayan-Das1 <dipayan1.das@gmai.com>
2020-12-19 21:31:05 +0530

    Code update

--- src/main/java/edu/spring/ldap/secureapp/SecureAppApplication.java
+++ src/main/java/edu/spring/ldap/secureapp/SecureAppApplication.java
@@ -2,6 +2,10 @@ package edu.spring.ldap.secureapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.ldap.repository.config.EnableLdapRepositories;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.core.support.LdapContextSource;
 
 @SpringBootApplication
 public class SecureAppApplication {
@@ -9,5 +13,7 @@ public class SecureAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SecureAppApplication.class, args);
 	}
+
+	
 
 }
